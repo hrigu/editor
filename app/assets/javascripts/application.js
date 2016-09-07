ecbece5cccd75adb55e7cc1ac89f08f857ff9d33
hrigu <chmuehlethaler@gmail.com>
2016-09-07 21:43:37 +0200

    wysiwig editor for comments

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,7 @@
 //= require turbolinks
 
 //= require 'bootstrap'
+
+//=require 'trix'
+
 //= require_tree .
--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -67,7 +67,7 @@ class PeopleController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
     def set_person
-      @person = Person.includes(:comments).find(params[:id])
+      @person = Person.includes(:comments).order('comments.id desc').find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
