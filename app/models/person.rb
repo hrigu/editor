34e90f244fdd43bc7d25a63940b254759add0556
hrigu <chmuehlethaler@gmail.com>
2016-09-10 11:11:59 +0200

    Kommentare: Sortierung nach id desc

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,8 +1,7 @@
 class Person < ApplicationRecord
 
-  has_many :comments
+  has_many :comments, -> { order 'id desc' }
 
   validates :s_n, :register_number, :first_name, :family_name, :lawyer, presence: true
-
 
 end
