61610722da051a1386831905bc6d81f5804bb8bb
hrigu <chmuehlethaler@gmail.com>
2016-09-11 20:54:18 +0200

    comment destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -54,14 +54,14 @@ class CommentsController < ApplicationController
   #
   # # DELETE /people/1
   # # DELETE /people/1.json
-  # def destroy
-  #   @person.destroy
-  #   respond_to do |format|
-  #     format.html { redirect_to people_url, notice: 'Person was successfully destroyed.' }
-  #     format.json { head :no_content }
-  #   end
-  # end
-  #
+  def destroy
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_to @person, notice: 'Comment was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
   private
   #
   # # Use callbacks to share common setup or constraints between actions.
