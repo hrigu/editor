d84fc74c95d559d285067646f7b3f56bd1f780c0
hrigu <chmuehlethaler@gmail.com>
2016-09-11 14:38:41 +0200

    edit comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,6 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
 
-  # GET /people
-  # GET /people.json
   def index
     @people = Person.all
   end
@@ -20,11 +18,10 @@ class CommentsController < ApplicationController
     @person = Person.find(params[:person_id])
     @comment = Comment.new
   end
-  #
-  # # GET /people/1/edit
-  # def edit
-  # end
-  #
+
+  def edit
+  end
+
   def create
     @person = Person.find(params[:person_id])
     @comment = Comment.new(comment_params)
@@ -41,20 +38,19 @@ class CommentsController < ApplicationController
       end
     end
   end
-  #
-  # # PATCH/PUT /people/1
-  # # PATCH/PUT /people/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @person.update(person_params)
-  #       format.html { redirect_to @person, notice: 'Person was successfully updated.' }
-  #       format.json { render :show, status: :ok, location: @person }
-  #     else
-  #       format.html { render :edit }
-  #       format.json { render json: @person.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
+
+  # PATCH/PUT /people/1
+  # PATCH/PUT /people/1.json
+  def update
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html { redirect_to([@person], notice: 'Comment was successfully updated.') }
+      else
+        format.html { render :edit }
+        format.json { render json: @person.errors, status: :unprocessable_entity }
+      end
+    end
+  end
   #
   # # DELETE /people/1
   # # DELETE /people/1.json
@@ -66,12 +62,14 @@ class CommentsController < ApplicationController
   #   end
   # end
   #
-  # private
+  private
   #
   # # Use callbacks to share common setup or constraints between actions.
-  # def set_person
-  #   @person = Person.find(params[:id])
-  # end
+  def set_comment
+     @person = Person.find(params[:person_id])
+     @comment = Comment.find(params[:id])
+
+  end
   #
   # # Never trust parameters from the scary internet, only allow the white list through.
   def comment_params
